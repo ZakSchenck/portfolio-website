0e58fae148fff720d9773d03c2b09ca70bd9df84
Joseph Dillon <joseph.dillon.522@gmail.com>
2022-02-26 21:28:17 -0500

    condensed js

--- app.js
+++ app.js
@@ -1,37 +1,19 @@
 // variables
-const aboutSectionOne = document.querySelector('.about-flex');
-const aboutSectionTwo = document.querySelector('.zakimg-flex');
-const headerContainer = document.querySelector('.header-container');
-const skillsSection = document.querySelector('.skills-section');
-const skillsImages = Array.from(document.querySelectorAll('.skills-img'))
-const gridItem = document.querySelector('.projects-grid');
-const grid = Array.from(document.querySelectorAll('.grid-item-container'));
+const skillsImages = Array.from(document.querySelectorAll('.skills-img'));
 const imgOverlay = Array.from(document.querySelectorAll('.ipp-img-overlay'));
 const projectInfo = Array.from(document.querySelectorAll('.project-info'));
 let screenPosition = window.innerHeight;
 
-window.addEventListener('scroll', (e) => {
-    let aboutPosition = aboutSectionOne.getBoundingClientRect().top;
-    let aboutPositionTwo = aboutSectionTwo.getBoundingClientRect().top;
-    let skillsPosition = skillsSection.getBoundingClientRect().top;
-    let gridItemPosition = gridItem.getBoundingClientRect().top;
+const elements = [
+    [document.querySelector('.about-flex'), 'about-animate'],
+    [document.querySelector('.zakimg-flex'), 'about-animate-two'],
+    [document.querySelector('.skills-section'), 'skills-animate'],
+    [document.querySelector('.projects-grid'), 'grid-animate']
+]
 
-    if (aboutPosition < screenPosition) {
-        aboutSectionOne.classList.add('about-animate');
-        aboutSectionOne.style.opacity = '1';
-    }
-    if (aboutPositionTwo < screenPosition) {
-        aboutSectionTwo.classList.add('about-animate-two');
-        aboutSectionTwo.style.opacity = '1';
-    }
-    if (skillsPosition < screenPosition) {
-        skillsSection.classList.add('skills-animate');
-        skillsSection.style.opacity = '1'
-    }
-    if (gridItemPosition < screenPosition) {
-        gridItem.classList.add('grid-animate');
-        gridItem.style.opacity = '1';
-    }
+window.addEventListener('scroll', (e) => {
+    elements.forEach(animateScroll);
+
     imgOverlay.forEach(img => {
         let gridPosition = img.getBoundingClientRect().top;
         if (gridPosition < screenPosition) {
@@ -54,4 +36,9 @@ window.addEventListener('scroll', (e) => {
 });
 
 
-
+const animateScroll = ([element, className]) => {
+    if (element.getBoundingClientRect().top < screenPosition) {
+        element.classList.add(className);
+        element.style.opacity = '1';
+    }
+}
